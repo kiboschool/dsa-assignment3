d64b1ce11a57a4d8c7feae67be2e2c0ec76b4ab4
Cody Doucette <doucette@bu.edu>
2023-05-15 20:58:08 -0400

    Add solution

--- llstring.py
+++ llstring.py
@@ -42,19 +42,69 @@ class LLString:
         return s
 
     def find(self, c):
-        pass
+        trav = self.head
+        index = 0
+        while trav is not None:
+            if trav.val == c:
+                return index
+            trav = trav.next
+            index += 1
+        return -1
+
+    def __to_upper_case(self, node):
+        if node is None:
+            return
+        node.val = node.val.upper()
+        self.__to_upper_case(node.next)
 
     def to_upper_case(self):
-        pass
+        self.__to_upper_case(self.head)
 
     def replace(self, old, new):
-        pass
+        trav = self.head
+        while trav is not None:
+            if trav.val == old:
+                trav.val = new
+            trav = trav.next
 
     def copy(self):
-        pass
+        return LLString(self.to_string())
 
     def trim(self):
-        pass
+        while self.head is not None and self.head.val == ' ':
+            self.head = self.head.next
+
+        if self.head is None:
+            return
+
+        trav = self.head
+        last_non_space = trav
+        while trav is not None:
+            if trav.val != ' ':
+                last_non_space = trav
+            trav = trav.next
+
+        self.tail = last_non_space
+        self.tail.next = None
+
+    def __find_nth(self, n, c, node):
+        if node is None:
+            return -1
+        if node.val == c and n == 1:
+            return 0
+
+        if node.val == c:
+            next_n = n - 1
+        else:
+            next_n = n
+
+        index_in_rest = self.__find_nth(next_n, c, node.next)
+        if index_in_rest == -1:
+            return -1
+        else:
+            return index_in_rest + 1
 
     def find_nth(self, n, c):
-        pass
+        if n < 1:
+            return -1
+        return self.__find_nth(n, c, self.head)
